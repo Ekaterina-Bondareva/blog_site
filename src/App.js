bdd1849d63a376ccc8e78212e7970bfb513153f4
bonivan <ibondarev@luxoft.com>
2022-10-31 12:09:10 -0400

    Adding react props

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
 import * as React from 'react';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://react.js.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
 //definition of App component
-const App = () => (
-  <div>
-    <h1>My Hacker Stories</h1>
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://react.js.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
-    <Search />
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
 
-    <hr />
+      <Search />
 
-    {/*creating an instance of List component */}
-    <List />
-    {/*creating another instance of List component */}
-    <List />
-  </div>
-);
+      <hr />
+
+      {/*creating an instance of List component */}
+      <List list={stories} />
+    </div>
+  );
+};
 
 const Search = () => {
   const handleChange = (event) => {
@@ -48,19 +48,23 @@ const Search = () => {
 };
 
 //definition of List component
-const List = () => (
+const List = (props) => (
   <ul>
-    {list.map((item) => (
-      <li key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.tetli}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-      </li>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
     ))}
   </ul>
+);
+
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
 );
 
 export default App;

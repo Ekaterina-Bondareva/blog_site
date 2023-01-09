20a8090487677e9f581d3d39cfaa19d46ac156d1
bonivan <ibondarev@luxoft.com>
2023-01-09 16:13:20 -0500

    Adding Fetching with React

--- src/App.js
+++ src/App.js
@@ -1,27 +1,6 @@
 import * as React from 'react';
-import { act } from 'react-dom/test-utils';
 
-const initialStories = [
-  {
-    title: 'React',
-    url: 'https://react.js.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-const getAsyncStories = () => 
-  new Promise((resolve, reject) => setTimeout(reject, 2000));
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
@@ -83,17 +62,18 @@ const App = () => {
     React.useEffect(() => {
       dispatchStories({type: 'STORIES_FETCH_INIT'});
 
-      getAsyncStories()
+      fetch(`${API_ENDPOINT}react`) 
+        .then((response) => response.json())
         .then((result) => {
           dispatchStories({
             type: 'STORIES_FETCH_SUCCESS',
-            payload: result.data.stories,
+            payload: result.hits,
           });
-      })
-      .catch(() => 
-        dispatchStories({type: 'STORIES_FETCH_FAILURE'})
-      );
-    }, []);
+        })
+        .catch(() =>
+          dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
+        );
+      }, []);
 
     const handleRemoveStory= (item) => {
       dispatchStories({

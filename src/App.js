584f0459336f0a0536388b2494db2beae94ee47e
bonivan <ibondarev@luxoft.com>
2023-01-08 20:47:33 -0500

    Adding imposible states

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { act } from 'react-dom/test-utils';
 
 const initialStories = [
   {
@@ -20,12 +21,7 @@ const initialStories = [
 ];
 
 const getAsyncStories = () => 
-  new Promise((resolve) =>
-    setTimeout(
-      () => resolve({data: {stories: initialStories}}),
-      2000
-    )
-  );
+  new Promise((resolve, reject) => setTimeout(reject, 2000));
 
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
@@ -41,14 +37,34 @@ const useSemiPersistentState = (key, initialState) => {
 
 const storiesReducer = (state, action) => {
   switch (action.type) {
-    case 'SET_STORIES':
-      return action.payload;
+    case 'STORIES_FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case 'STORIES_FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case 'STORIES_FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
     case 'REMOVE_STORY':
-      return  state.filter(
-        (story) => action.payload.objectID !== story.objectID
-      );
-      default: 
-        throw new Error();
+      return {
+        ...state,
+        data: state.data.filter(
+          (story) => action.payload.objectID !== story.objectID
+        ),
+      };
+    default:
+      throw new Error();
   }
 };
 
@@ -61,25 +77,22 @@ const App = () => {
 
   const [stories, dispatchStories] = React.useReducer(
     storiesReducer,
-    []
+    {data: [], isLoading: false, isError: false}
   );
 
-  const [isLoading, setIsLoading] = React.useState(false);
-  
-  const [isError, setIsError] = React.useState(false);
-
     React.useEffect(() => {
-      setIsLoading(true);
+      dispatchStories({type: 'STORIES_FETCH_INIT'});
 
       getAsyncStories()
         .then((result) => {
           dispatchStories({
-            type: 'SET_STORIES',
+            type: 'STORIES_FETCH_SUCCESS',
             payload: result.data.stories,
-          })
-          setIsLoading(false);
+          });
       })
-      .catch(() => setIsError(true));
+      .catch(() => 
+        dispatchStories({type: 'STORIES_FETCH_FAILURE'})
+      );
     }, []);
 
     const handleRemoveStory= (item) => {
@@ -93,10 +106,10 @@ const App = () => {
     setSearchTerm(event.target.value);
   };
 
-  const searchedStories = stories.filter((story) =>
+  const searchedStories = stories.data.filter((story) => 
     story.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
-
+  
   return (
     <div>
       <h1>My Hacker Stories</h1>
@@ -113,9 +126,9 @@ const App = () => {
 
       <hr />
 
-      {isError && <p>Something went wrong ...</p>}
+      {stories.isError && <p>Something went wrong...</p>}
 
-      {isLoading ? (
+      {stories.isLoading ? (
         <p>Loading ...</p>
       ) : (
         /* creating an instance of List component */

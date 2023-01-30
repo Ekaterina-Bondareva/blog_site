e484fef05e0c0bc6c9ed0a8d79681f52f08933ee
bonivan <ibondarev@luxoft.com>
2023-01-30 10:14:37 -0500

    Adding Testing in React (TS, TC and Assertions)

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe('something truthy and falsy', () => {
+  it('true to be true', () => {
+    expect(true).toBeTruthy();
+  });
+
+  it('false to be false', () => {
+    expect(false).toBeFalsy();
+  })
+});
+
+describe('App component', () => {
+  test('removes an item when clicking the Dismiss button', () => {
+
+  });
+
+  test('requests some initial stories from an API', () => {
+
+  });
 });

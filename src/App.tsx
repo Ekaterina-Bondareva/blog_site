bfba31503677468249132325e398c43632d6c34f
bonivan <ibondarev@luxoft.com>
2023-02-03 10:23:15 -0500

    Adding React Project Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import * as React from 'react';
 import axios from 'axios';
 
+import { SearchForm } from './SearchForm';
+import { List } from './List';
+import type {Story, Stories} from './List';
+
 import './App.css';
-import { ReactComponent as Check } from './check.svg';
-import { type } from '@testing-library/user-event/dist/type';
+
+// import { type } from '@testing-library/user-event/dist/type';
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
-type Story = {
-  objectID: string;
-  url: string;
-  title: string;
-  author: string;
-  num_comments: number;
-  points: number;
-};
-
-type Stories = Array<Story>;
-
 const useSemiPersistentState = (
-    key: string, 
-    initialState: string
+  key: string, 
+  initialState: string
 ) : [string, (newValue: string) => void] => {
   const isMounted = React.useRef(false);
 
@@ -104,37 +97,6 @@ const storiesReducer = (
       throw new Error();
   }
 };
-
-type SearchFormProps = {
-  searchTerm: string;
-  onSearchInput: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  onSearchSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
-};
-
-const SearchForm = ({
-  searchTerm,
-  onSearchInput,
-  onSearchSubmit,
-}: SearchFormProps) => (
-  <form onSubmit={onSearchSubmit} className='search-form'>
-    <InputWithLabel
-      id='search'
-      value={searchTerm}
-      isFocused
-      onInputChange={onSearchInput}
-    >
-      <strong>Search:</strong>
-    </InputWithLabel>
-
-    <button 
-      type='submit' 
-      disabled={!searchTerm}
-      className='button button_large'
-    >
-      Submit
-    </button>
-  </form>
-);
 
 const getSumComments = (stories: {data: Stories, isLoading: boolean, isError: boolean}) => {
   return stories.data.reduce(
@@ -224,95 +186,6 @@ const App = () => {
   );
 };
 
-type InputWithLabelProps = {
-  id: string;
-  value: string;
-  type?: string;
-  onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  isFocused?: boolean;
-  children: React.ReactNode;
-};
-
-const InputWithLabel = ({
-  id, 
-  value, 
-  type = "text",
-  onInputChange,
-  isFocused,
-  children,
-}: InputWithLabelProps) => {
-  const inputRef = React.useRef<HTMLInputElement>(null!);
-
-  React.useEffect(() => {
-    if (isFocused && inputRef.current) {
-      inputRef.current.focus();
-    }
-  }, [isFocused]);
-
-  return (
-    <>
-      <label htmlFor={id} className='label'>
-        {children}
-      </label>
-      &nbsp;
-      <input 
-        ref={inputRef}
-        id={id} 
-        type={type} 
-        value={value}
-        onChange={onInputChange} 
-        className='input'
-      />
-    </>
-  );
-};
-
-type ListProps = {
-  list: Stories;
-  onRemoveItem: (item: Story) => void;
-};
-
-//definition of List component
-const List = React.memo(
-({list, onRemoveItem}: ListProps) => (
-  (
-    <ul>
-      {list.map((item) => (
-        <Item 
-          key={item.objectID}
-          item={item} 
-          onRemoveItem={onRemoveItem}
-        />
-      ))}
-    </ul>
-  )
-));
-
-type ItemProps = {
-  item: Story;
-  onRemoveItem: (item: Story) => void;
-};
-
-const Item = ({item, onRemoveItem}: ItemProps) => (
-  <li className='item'>
-    <span style={{width: '40%'}}>
-      <a href={item.url}>{item.title}</a>
-    </span>
-    <span style={{width: '30%'}}>{item.author}</span>
-    <span style={{width: '10%'}}>{item.num_comments}</span>
-    <span style={{width: '10%'}}>{item.points}</span>
-    <span style={{width: '10%'}}>
-      <button 
-        type="button" 
-        onClick={() => onRemoveItem(item)}
-        className='button button_small'
-      >
-        <Check height='18px' width='18px' />
-      </button>
-    </span>
-  </li>
-);
-
 export default App;
 
-export {storiesReducer, SearchForm, InputWithLabel, List, Item};
+export {storiesReducer, SearchForm, List};
